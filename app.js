d99dfe865f3b92e80da9626bc6a577005fa70486
Cameron Knight <ckknight@gmail.com>
2014-04-11 21:04:48 -0700

    remove mongo and websockets references, add some other libraries

--- app.js
+++ app.js
@@ -4,30 +4,23 @@
  * Entry point for KOAN app. Initiates database connection and starts listening for requests on configured port.
  */
 
+require('es6-shim');
 var config = require('./server/config/config'),
-    mongo = require('./server/config/mongo'),
-    mongoSeed = require('./server/config/mongo-seed'),
-    koaConfig = require('./server/config/koa'),
-    ws = require('./server/config/ws'),
-    co = require('co'),
-    koa = require('koa'),
-    app = koa();
+  koaConfig = require('./server/config/koa'),
+  co = require('co'),
+  koa = require('koa'),
+  app = koa();
 
 module.exports = app;
 
-app.init = co(function *() {
-  // initialize mongodb and populate the database with seed data if empty
-  yield mongo.connect();
-  yield mongoSeed();
-
+app.init = co(function * () {
   // koa config
   koaConfig(app);
 
-  // create http and websocket servers and start listening for requests
+  // create http and start listening for requests
   app.server = app.listen(config.app.port);
-  ws.listen(app.server);
   if (config.app.env !== 'test') {
-    console.log('KOAN listening on port ' + config.app.port);
+    console.log(config.app.name + ' listening on port ' + config.app.port);
   }
 });
 
--- server/config/koa.js
+++ server/config/koa.js
@@ -1,48 +1,56 @@
 'use strict';
 
 var fs = require('fs'),
-    logger = require('koa-logger'),
-    send = require('koa-send'),
-    jwt = require('koa-jwt'),
-    config = require('./config'),
-    passport = require('./passport');
+  logger = require('koa-logger'),
+  etag = require('koa-etag'),
+  serve = require('koa-static'),
+  json = require('koa-json'),
+  session = require('koa-session'),
+  compress = require('koa-compress'),
+  err = require('koa-err'),
+  responseTime = require('koa-response-time'),
+  conditional = require('koa-conditional-get'),
+  config = require('./config'),
+  path = require('path');
 
 module.exports = function (app) {
   // middleware configuration
+  app.use(responseTime());
+  app.use(err());
+
   if (config.app.env !== 'test') {
     app.use(logger());
   }
+
+  app.use(conditional());
+  app.use(compress());
+  app.use(etag());
   if (config.app.env === 'development') {
-    app.use(require('./koa-livereload')({excludes: ['/modules']}));
+    app.use(require('koa-livereload')({
+      excludes: ['/modules']
+    }));
   }
-  app.use(passport.initialize());
-  app.use(passport.session());
 
-  // mount passport oauth routes
-  passport.routes(app);
-
-  // register publicly accessible api endpoint. this is useful for special cases like login, user profile images, etc.
-  require('../controllers/public').init(app);
+  if (config.app.env !== 'production') {
+    app.use(json());
+  }
 
   // serve the angular static files from the /client directory, use caching (7 days) only in production
   // if the file is not found and requested path is not /api, serve index.html page and let angular handle routing
-  var sendOpts = config.app.env === 'production' ? {root: 'client', maxage: 1000 * 60 * 60 * 24 * 7} : {root: 'client'};
-  app.use(function *(next) {
-    if (this.path.substr(0, 5).toLowerCase() === '/api/') {
-      yield next;
-    } else {
-      if (yield send(this, this.path, sendOpts)) {
-        return;
-      }
-      yield send(this, '/index.html', sendOpts);
-    }
-  });
+  var sendOpts = config.app.env === 'production' ? {
+    root: 'client',
+    maxage: 1000 * 60 * 60 * 24 * 7
+  } : {
+    root: 'client'
+  };
 
-  // middleware below this line is only reached if jwt token is valid
-  app.use(jwt({secret: config.app.secret}));
+  app.use(serve(path.join(__dirname, '../../client')));
+
+  app.keys = [config.secret];
+  app.use(session());
 
   // mount all the routes defined in the api controllers
-  fs.readdirSync('./server/controllers').forEach(function (file) {
+  fs.readdirSync(path.join(__dirname, '../controllers')).forEach(function (file) {
     require('../controllers/' + file).init(app);
   });
 };
